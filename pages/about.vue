c71aed81058d48a383aa0b429bc84c6c24e23d95
hyunkyuchoi <chlgusrb6@gmail.com>
2018-05-09 20:10:01 +0900

    Javascript_2018-1_Nuxt.js

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template> 
+   <section class="section"> 
+       <div class="block"> 
+           about page 
+       </div> 
+   </section> 
+     
+ </template> 
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="section">
+    <div class="block">
+      <a href="#" class="button is-primary">Test</a><br>  
+      <p>최현규의 포토폴리오</p>  
+      <img src="~static/mok.jpg" alt=""><br>  
+       <img src="~assets/mok2.png" alt=""> 
+
+    </div>
+  </section>
+</template>
